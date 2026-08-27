f2c68ceffea665453beadd02b8ff34d4c3058400
MarkHA <marck.hernandez@epn.edu.ec>
2026-08-27 01:06:46 -0500

    Activación página 27/08/2026

--- src/components/sweet/ConfirmModal.tsx
+++ src/components/sweet/ConfirmModal.tsx
@@ -90,7 +90,7 @@ export const ConfirmModal = ({ open, onOpenChange, cart, products, onConfirm }: 
               <DialogDescription className="text-base">
                 Para garantizar la calidad y textura perfecta de tus bolos, todos los pedidos se preparan y congelan adecuadamente.
                 <br /><br />
-                <strong className="text-foreground">Tu pedido será entregado el 25/08/26 a partir de la 14:00.</strong>
+                <strong className="text-foreground">Tu pedido será entregado el 28/08/26 a partir de la 09:00.</strong>
               </DialogDescription>
             </DialogHeader>
             <DialogFooter className="w-full gap-2 mt-6 sm:gap-2">
